26eb5895cfba56d83d75c89543a187c3c7d46764
ShironiDesu <119896050+ShironiDesu@users.noreply.github.com>
2024-05-06 23:51:26 +0500

    Final version

--- js/main.js
+++ js/main.js
@@ -530,6 +530,7 @@ const shuffleArray = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
+    console.log(i);
   }
 };
 
